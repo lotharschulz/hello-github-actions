1296cad71d20198e21493c50f626bcf48dfe1482
Lothar Schulz <lothar.sch@gmx.de>
2019-05-02 08:28:39 +0200

    unfail tests

--- hello_test.go
+++ hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	expected := "Hello, World!---fail"
+	expected := "Hello, World!"
 	if observed := HelloWorld(); observed != expected {
 		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
 	}
